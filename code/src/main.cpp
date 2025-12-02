abdb1711ed5eaaef6fd85a61e11816aca380cd6d
Patrick Nelson <supermariokart98@gmail.com>
2025-12-01 22:13:52 -0500

    Update pause logic to be based on upper limit instead of lower

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -166,7 +166,7 @@ void initPauseUnpauseAction() {
 
   // If the tonearm is not making contact with the lift (implying it's on a record or home), OR the tonearm is at the lower limit (below a record), then "pause" (or lift it up)
   // TODO: Pull this value from home calibration. TEST_VERTICAL_LOWER_LIMIT is only for testing.
-  if(digitalRead(Pin::Lift) == LiftStatus::SetDown || verticalPosition - ENCODER_DELTA <= TEST_VERTICAL_LOWER_LIMIT) {
+  if(digitalRead(Pin::Lift) == LiftStatus::SetDown || !(verticalPosition + ENCODER_DELTA >= TEST_VERTICAL_UPPER_LIMIT)) {
     movementStepper.setSpeed(12);
     actionCommand = ActionCommand::Pause;
   } else {
