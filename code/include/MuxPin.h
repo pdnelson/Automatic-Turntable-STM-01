1f57a612038e1d95e0a1104c83d2f519fd546859
Patrick Nelson <supermariokart98@gmail.com>
2026-03-02 08:59:05 -0500

    Rearrange MuxPin entries to match schematic changes

--- code/include/MuxPin.h
+++ code/include/MuxPin.h
@@ -2,18 +2,12 @@
 #define MUXPIN_H
 
 enum MuxPin : uint8_t {
-    // Action Buttons
-    BtnPlay = 2,
-    BtnPause = 3,
-    BtnTestMode = 4,
-    BtnCalibration = 7,
-
-    // Settings Buttons
-    BtnSizeSelect = 0,
+    BtnPlay = 0,
     BtnSpeedSelect = 1,
-    BtnResetSettings = 6,
-
-    Unused5 = 5
+    BtnSizeSelect = 2,
+    BtnPause = 3,
+    BtnCalibration = 4,
+    BtnTestMode = 7,
 };
 
 #endif
