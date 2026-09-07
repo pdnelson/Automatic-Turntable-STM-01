b3a2cf4450ba1bfdf56e12058334c8d6858bdc11
Patrick Nelson <supermariokart98@gmail.com>
2026-09-07 18:58:28 -0400

    Remove hard-coded 10in and 12in values

--- code/src/persistence/CalibrationValues.cpp
+++ code/src/persistence/CalibrationValues.cpp
@@ -8,8 +8,6 @@ CalibrationValues::CalibrationValues() {
 void CalibrationValues::load() {
     // to do
     home = 2045;
-    in12 = 2983;
-    in10 = 3257;
     verticalUpperLimit = TEST_VERTICAL_UPPER_LIMIT;
     verticalLowerLimit = TEST_VERTICAL_LOWER_LIMIT;
     polarity = StmEncoderPolarity::REVERSED;
